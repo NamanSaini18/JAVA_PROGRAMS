a9ab7fa9e45ad702a4440744020f9db996438c56
Naman Saini <81188700+NamanSaini18@users.noreply.github.com>
2021-11-13 20:38:59 +0530

    Create Diamond_Pattern.java

---
+++ Diamond_Pattern.java
@@ -0,0 +1,44 @@
+// This is the code for printing the Diamond Pattern of Stars.
+import java.util.Scanner;
+
+public class Diamond_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output of this code will be:
+// 6
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
