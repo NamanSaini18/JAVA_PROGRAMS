ec75d8ebf261bdf79b139706288f27a1f49a3277
Naman Saini <naman.saini_cs20@gla.ac.in>
2022-01-10 23:12:47 +0530

    Create Codechef_DIFFSUM.java

---
+++ Codechef_DIFFSUM.java
@@ -0,0 +1,21 @@
+// This is the solution of Codechef problem named DIFFSUM
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a>b){
+		    System.out.println(a-b);
+		}
+		else{
+		    System.out.println(a+b);
+		}
+	}
+}
